18304ed0c9044f1955922a226f5e1d426cca8936
2613710-alt <2613710@sookmyung.ac.kr>
2026-09-17 16:53:24 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,18 @@
 
 int main(void)
 {
-    char c;
+    float a, b;
+    float result;
 
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("분자를 입력하세요 : ");
+    scanf("%f", &a);
 
-    printf("the next character of %c (%i) is %c (%i)\n",
-           c, c, c + 1, c + 1);
+    printf("분모를 입력하세요 : ");
+    scanf("%f", &b);
+
+    result = a / b;
+
+    printf("나누기의 결과는 %f 입니다.\n", result);
 
     return 0;
 }
